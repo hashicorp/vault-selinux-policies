1798f69abfe3c2ef20961d4db1c225976c4a853d
xntrik <xntrik@gmail.com>
2020-11-12 13:20:37 +0800

    test: Ensure that we test the bools against the correct container

--- products/vault_selinux/ci/validate.sh
+++ products/vault_selinux/ci/validate.sh
@@ -36,7 +36,7 @@ docker exec $FEDORA_ID dnf install -y /app/$(ls products/*/*fc31.noarch.rpm)
 
 docker exec $FEDORA_ID bash -c 'semanage module -l | grep vault'
 docker exec $FEDORA_ID bash -c 'semanage port -l | grep vault_cluster_port_t'
-docker exec $CENTOS_ID bash -c 'semanage boolean -l | grep vault_outbound'
+docker exec $FEDORA_ID bash -c 'semanage boolean -l | grep vault_outbound'
 docker exec $FEDORA_ID dnf remove -y vault_selinux
 
 docker stop $FEDORA_ID
