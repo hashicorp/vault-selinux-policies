75a7d6442751967d1c59071d43003fb4aefe8c2d
xntrik <xntrik@gmail.com>
2020-11-12 12:51:44 +0800

    test: added boolean tests to ci/validate.sh

--- products/vault_selinux/ci/validate.sh
+++ products/vault_selinux/ci/validate.sh
@@ -21,6 +21,8 @@ docker exec $CENTOS_ID yum install -y /app/$(ls products/*/*el8.noarch.rpm)
 
 docker exec $CENTOS_ID bash -c 'semanage module -l | grep vault'
 docker exec $CENTOS_ID bash -c 'semanage port -l | grep vault_cluster_port_t'
+docker exec $CENTOS_ID bash -c 'setsebool vault_outbound_udp_dns on'
+docker exec $CENTOS_ID bash -c 'setsebool vault_outbound_http on'
 docker exec $CENTOS_ID yum remove -y vault_selinux
 
 docker stop $CENTOS_ID
@@ -28,14 +30,15 @@ docker stop $CENTOS_ID
 # Run Fedora rpm validation
 FEDORA_ID=$(docker run -d -v $WORKDIR:/app -e HC_PRODUCT=$HC_PRODUCT -e HC_VERSION=$HC_VERSION \
   --entrypoint="" -w="/app" --privileged $IMAGE_RPM_SYSTEM /usr/sbin/init)
-# Wait for CentOS to spin up
+# Wait for Fedora to spin up
 sleep 1
 docker exec $FEDORA_ID dnf install -y libselinux-utils policycoreutils policycoreutils-python-utils selinux-policy-targeted
 docker exec $FEDORA_ID dnf install -y /app/$(ls products/*/*fc31.noarch.rpm)
-# docker exec $FEDORA_ID dnf install -y /app/$(ls products/*/*.noarch.rpm)
 
 docker exec $FEDORA_ID bash -c 'semanage module -l | grep vault'
 docker exec $FEDORA_ID bash -c 'semanage port -l | grep vault_cluster_port_t'
+docker exec $CENTOS_ID bash -c 'setsebool vault_outbound_udp_dns on'
+docker exec $CENTOS_ID bash -c 'setsebool vault_outbound_http on'
 docker exec $FEDORA_ID dnf remove -y vault_selinux
 
 docker stop $FEDORA_ID
