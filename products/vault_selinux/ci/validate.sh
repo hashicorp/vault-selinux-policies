35083feadb16d23d5e5f3e3f30849ea2b3bde061
xntrik <xntrik@gmail.com>
2020-11-17 07:43:12 +0800

    tests: testing with centos7 as well

--- products/vault_selinux/ci/validate.sh
+++ products/vault_selinux/ci/validate.sh
@@ -26,6 +26,20 @@ docker exec $CENTOS_ID yum remove -y vault_selinux
 
 docker stop $CENTOS_ID
 
+CENTOS_ID=$(docker run -d -v $WORKDIR:/app -e HC_PRODUCT=$HC_PRODUCT -e HC_VERSION=$HC_VERSION \
+  --entrypoint="" -w="/app" --privileged $IMAGE_CENTOS7_SYSTEM /usr/sbin/init)
+# Wait for CentOS to spin up
+sleep 1
+docker exec $CENTOS_ID yum install -y libselinux-utils policycoreutils policycoreutils-python-utils selinux-policy-targeted
+docker exec $CENTOS_ID yum install -y /app/$(ls products/*/*el8.noarch.rpm)
+
+docker exec $CENTOS_ID bash -c 'semanage module -l | grep vault'
+docker exec $CENTOS_ID bash -c 'semanage port -l | grep vault_cluster_port_t'
+docker exec $CENTOS_ID bash -c 'semanage boolean -l | grep vault_outbound'
+docker exec $CENTOS_ID yum remove -y vault_selinux
+
+docker stop $CENTOS_ID
+
 # Run Fedora rpm validation
 FEDORA_ID=$(docker run -d -v $WORKDIR:/app -e HC_PRODUCT=$HC_PRODUCT -e HC_VERSION=$HC_VERSION \
   --entrypoint="" -w="/app" --privileged $IMAGE_RPM_SYSTEM /usr/sbin/init)
