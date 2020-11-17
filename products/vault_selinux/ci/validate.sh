ec1b7b6ecb6e0f927ffe1ad8e672cfebf3dffd08
xntrik <xntrik@gmail.com>
2020-11-17 10:33:00 +0800

    tests: added Fedora32

--- products/vault_selinux/ci/validate.sh
+++ products/vault_selinux/ci/validate.sh
@@ -41,9 +41,9 @@ docker exec $CENTOS_ID yum remove -y vault_selinux
 
 docker stop $CENTOS_ID
 
-# Run Fedora rpm validation
+# Run Fedora31 rpm validation
 FEDORA_ID=$(docker run -d -v $WORKDIR:/app -e HC_PRODUCT=$HC_PRODUCT -e HC_VERSION=$HC_VERSION \
-  --entrypoint="" -w="/app" --privileged -ti $IMAGE_RPM_SYSTEM /bin/bash)
+  --entrypoint="" -w="/app" --privileged -ti $IMAGE_F31_SYSTEM /bin/bash)
 # Wait for Fedora to spin up
 sleep 1
 docker exec $FEDORA_ID dnf install -y libselinux-utils policycoreutils policycoreutils-python-utils selinux-policy-targeted
@@ -56,4 +56,18 @@ docker exec $FEDORA_ID dnf remove -y vault_selinux
 
 docker stop $FEDORA_ID
 
+# Run Fedora32 rpm validation
+FEDORA_ID=$(docker run -d -v $WORKDIR:/app -e HC_PRODUCT=$HC_PRODUCT -e HC_VERSION=$HC_VERSION \
+  --entrypoint="" -w="/app" --privileged -ti $IMAGE_F32_SYSTEM /bin/bash)
+# Wait for Fedora to spin up
+sleep 1
+docker exec $FEDORA_ID dnf install -y libselinux-utils policycoreutils policycoreutils-python-utils selinux-policy-targeted
+docker exec $FEDORA_ID dnf install -y /app/$(ls products/*/*fc32.noarch.rpm)
+
+docker exec $FEDORA_ID bash -c 'semanage module -l | grep vault'
+docker exec $FEDORA_ID bash -c 'semanage port -l | grep vault_cluster_port_t'
+docker exec $FEDORA_ID bash -c 'semanage boolean -l | grep vault_outbound'
+docker exec $FEDORA_ID dnf remove -y vault_selinux
+
+docker stop $FEDORA_ID
 echo "Validation tests complete."
